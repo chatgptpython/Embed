31028511ba7ee298a402594bb363628e609d1260
chatgptpython <142637375+chatgptpython@users.noreply.github.com>
2023-08-26 15:04:30 +0200

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -214,7 +214,7 @@ document.addEventListener("DOMContentLoaded", function() {
             </div>
         </div>
         <div id="chatbot-icon" onclick="toggleChat()">
-            <span>💬</span>
+            <span>🤖</span>
         </div>
     `;
     var div = document.createElement('div');
