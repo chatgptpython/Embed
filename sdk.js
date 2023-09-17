9d78bc8f97301372780626dce38bfaf49922f636
chatgptpython <142637375+chatgptpython@users.noreply.github.com>
2023-09-17 18:21:33 +0700

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
         
    #chatbot {
             position: fixed;
-            bottom: 85px !important;
+            bottom: 90px !important;
             right: 30px;
             width: 420px;
             height: 640px;
