8c84a19d00d324aba1a55db013cd639e1c97af8f
chatgptpython <142637375+chatgptpython@users.noreply.github.com>
2023-09-19 14:49:17 +0700

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -376,6 +376,18 @@ document.addEventListener("DOMContentLoaded", function() {
 };
 
 
+async function fetchTitleMessage() {
+    try {
+        const response = await fetch(`${backendUrl}/get_title_message`);
+        const data = await response.json();
+        if (data.message) {
+            document.querySelector("#chatbot header").innerText = data.message;
+        }
+    } catch (error) {
+        console.error("Failed to fetch title message:", error);
+    }
+}
+
 window.toggleChat = async function() {
     const chatbot = document.getElementById("chatbot");
     const icon = document.getElementById("chatbot-icon");
@@ -385,7 +397,10 @@ window.toggleChat = async function() {
         setTimeout(function() {
             chatbot.classList.add("visible");
         }, 50);
-        
+
+        // Ophalen en instellen van het titelbericht
+        await fetchTitleMessage();
+
         if (firstTimeOpen) {
             await typeWelcomeMessage(); // Nu kan 'await' hier gebruikt worden
             firstTimeOpen = false;
@@ -499,9 +514,10 @@ window.sendMessage = function() {
     document.getElementById("user-input").onkeyup = function(event) {
         handleKeyUp(event);
     };
-
+        
 if(window.innerWidth > 768) {
-    setTimeout(function() {
+    setTimeout(async function() {
+        await fetchTitleMessage();
         toggleChat();
     }, 3000);  
 }
