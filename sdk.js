aa1a5f68af9ab364fbde6ffd978cb09da6235285
chatgptpython <142637375+chatgptpython@users.noreply.github.com>
2023-10-29 16:10:12 +0100

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -189,8 +189,8 @@ document.addEventListener("DOMContentLoaded", function() {
     background-color: green;
     border-radius: 50%;
     border: 2px solid white;
-    bottom: 10px;  /* Iets verhoogd om het bolletje naar boven te verschuiven */
-    right: 10px;  /* Iets verhoogd om het bolletje naar links te verschuiven */
+    bottom: -2px;  /* Verlaagd om het bolletje verder naar beneden te verschuiven */
+    right: 2px;  /* Verlaagd om het bolletje verder naar links te verschuiven */
 }
 
 
