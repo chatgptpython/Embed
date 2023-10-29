2a6d4e5ab52aaedd1e858c361a382bfb74146676
chatgptpython <142637375+chatgptpython@users.noreply.github.com>
2023-10-29 18:04:35 +0100

    Update SDKHeikant.js

--- SDKHeikant.js
+++ SDKHeikant.js
@@ -228,7 +228,7 @@ document.addEventListener("DOMContentLoaded", function() {
     #chatbot-title {
         line-height: 1;  /* Minimale ruimte tussen de regels */
         margin-bottom: 0;  /* Geen extra ruimte onder de titel */
-        font-size: 0.8em;  /* Verkleinde titelgrootte */
+        font-size: 0.8em !important;
     }
 }
 
