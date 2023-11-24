8c436cfae95c0333e7f6d450f90dbbe1dcafdcf1
chatgptpython <142637375+chatgptpython@users.noreply.github.com>
2023-11-24 17:53:13 +0100

    Update sdk1.js

--- sdk1.js
+++ sdk1.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function() {
             transform: scale(0.8); 
             position: fixed;
             bottom: 20px;
-            right: 30px;
+            right: 15px;
             width: 30px; /* Aangepaste breedte */
             height: 30px; /* Aangepaste hoogte */
             border-radius: 50%;
