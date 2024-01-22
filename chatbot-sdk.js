dcd1c22f07890ec30049765856f7b6b5362d8939
chatgptpython <142637375+chatgptpython@users.noreply.github.com>
2024-01-22 12:23:47 +0100

    Update chatbot-sdk.js

--- chatbot-sdk.js
+++ chatbot-sdk.js
@@ -699,8 +699,7 @@ document.addEventListener("DOMContentLoaded", function() {
         <div id="chatbot">
             <header>
                 <div class="icon-container">
-                    <img src="https://raw.githubusercontent.com/chatgptpython/embed/main/chat.png">
-                    <span class="online-indicator"></span>
+                    <img src="https://raw.githubusercontent.com/chatgptpython/embed/main/ster.png">
                 </div>
                 <div id="chatbot-title-container">
                     <span id="chatbot-title">
